d365d199cf0922031da8b06dfc6e747ab364d9e4
Nguyen Van Tam <tamvannguyen200795@gmail.com>
2021-02-21 21:59:06 +0700

    Fixed testing

--- part2-pytorch/models/my_model.py
+++ part2-pytorch/models/my_model.py
@@ -9,30 +9,26 @@ class MyModel(nn.Module):
         #############################################################################
         # TODO: Initialize the network weights                                      #
         #############################################################################
-        self.features = nn.Sequential(
-            nn.Conv2d(3, 64, 3, padding=1),
-            nn.MaxPool2d(2, 2),
-            nn.Conv2d(64, 128, 3, padding=1),
-            nn.MaxPool2d(2, 2),
-            nn.Conv2d(128, 256, 3, padding=1),
-            nn.Conv2d(256, 256, 3, padding=1),
-            nn.MaxPool2d(2, 2),
-            nn.Conv2d(256, 512, 3, padding=1),
-            nn.Conv2d(512, 512, 3, padding=1),
-            nn.MaxPool2d(2, 2),
-            nn.Conv2d(512, 512, 3, padding=1),
-            nn.Conv2d(512, 512, 3, padding=1),
-            nn.MaxPool2d(2, 2),
-        )
-        self.classifier = nn.Sequential(
-            nn.Dropout(),
-            nn.Linear(512, 512),
-            nn.ReLU(True),
-            nn.Dropout(),
-            nn.Linear(512, 512),
-            nn.ReLU(True),
-            nn.Linear(512, 10),
-        )
+        self.conv1_1 =  nn.Conv2d(3, 64, 3, padding=1)
+        self.pool1 = nn.MaxPool2d(2, 2)
+        self.conv2_1 = nn.Conv2d(64, 128, 3, padding=1)
+        self.pool2 = nn.MaxPool2d(2, 2)
+        self.conv3_1 = nn.Conv2d(128, 256, 3, padding=1)
+        self.conv3_2 = nn.Conv2d(256, 256, 3, padding=1)
+        self.pool3 = nn.MaxPool2d(2, 2)
+        self.conv4_1 = nn.Conv2d(256, 512, 3, padding=1)
+        self.conv4_2 = nn.Conv2d(512, 512, 3, padding=1)
+        self.pool4 = nn.MaxPool2d(2, 2)
+        self.conv5_1 = nn.Conv2d(512, 512, 3, padding=1)
+        self.conv5_2 = nn.Conv2d(512, 512, 3, padding=1)
+        self.pool5 = nn.MaxPool2d(2, 2)
+        self.dropout1 = nn.Dropout()
+        self.fc1 = nn.Linear(512, 512)
+        self.relu1 = nn.ReLU(True)
+        self.dropout2 = nn.Dropout()
+        self.fc2 = nn.Linear(512, 512)
+        self.fc3 = nn.Linear(512, 10)
+
          # Initialize weights
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
@@ -48,9 +44,25 @@ class MyModel(nn.Module):
         #############################################################################
         # TODO: Implement forward pass of the network                               #
         #############################################################################
-        x = self.features(x)
+        x = self.conv1_1(x)
+        x = self.pool1(x)
+        x = self.conv2_1(x)
+        x = self.pool2(x)
+        x = self.conv3_1(x)
+        x = self.conv3_2(x)
+        x = self.pool3(x)
+        x = self.conv4_1(x)
+        x = self.conv4_2(x)
+        x = self.pool4(x)
+        x = self.conv5_1(x)
+        x = self.conv5_2(x)
+        x = self.pool5(x)
         x = x.view(x.size(0), -1)
-        outs = self.classifier(x)
+        x = self.dropout1(x)
+        x = F.relu(self.fc1(x))
+        x = self.dropout2(x)
+        x = F.relu(self.fc2(x))
+        outs = self.fc3(x)
         #############################################################################
         #                              END OF YOUR CODE                             #
         #############################################################################
